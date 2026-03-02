d964fb6263b20b93efec26d3369c8eedf64c725f
louisedias <louisedias9@gmail.com>
2026-03-02 19:11:38 -0300

    "getter and setter Product.java"

--- sistema-pedidos/src/main/java/com/louise/sistema_pedidos/model/Product.java
+++ sistema-pedidos/src/main/java/com/louise/sistema_pedidos/model/Product.java
@@ -16,4 +16,35 @@ public class Product {
     private Double price;
     private Integer stockQuantity;
 
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public Integer getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setId (Long id) {
+        this.id = id;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public void setPrice (Double price) {
+        this.price = price;
+    }
+
+    public void setStockQuantity (Integer stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
 }
